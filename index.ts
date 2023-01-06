dfb80efea87b8639272716334292aaff6a3c0a62
samudurand <samudurand@gmail.com>
2023-01-06 14:50:11 +0100

    Introducing Konva

--- index.ts
+++ index.ts
@@ -1,6 +1,33 @@
 // Import stylesheets
+import Konva from 'konva';
 import './style.css';
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+appDiv.innerHTML = `<h1>TypeScript Starter</h1><div id="container"></div>`;
+
+var stage = new Konva.Stage({
+  container: 'container',   // id of container <div>
+  width: 500,
+  height: 500
+});
+
+var layer = new Konva.Layer();
+
+var circle = new Konva.Circle({
+  x: stage.width() / 2,
+  y: stage.height() / 2,
+  radius: 70,
+  fill: 'red',
+  stroke: 'black',
+  strokeWidth: 4
+});
+
+// add the shape to the layer
+layer.add(circle);
+
+// add the layer to the stage
+stage.add(layer);
+
+// draw the image
+layer.draw();
